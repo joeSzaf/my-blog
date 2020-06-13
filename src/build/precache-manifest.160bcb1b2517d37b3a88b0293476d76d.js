8efb6859786dda91114f7b45c976abfb01aa37e5
Joe Szaf <joeszaf+github@gmail.com>
2020-06-13 14:50:03 -0400

    First commit

---
+++ src/build/precache-manifest.160bcb1b2517d37b3a88b0293476d76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e2bad3eecc8b3dcde865932e46a98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da3bfd60543a2f8e38e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "313bbc692f6be1b4c269",
+    "url": "/static/js/2.7a6e94b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a6e94b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da3bfd60543a2f8e38e",
+    "url": "/static/js/main.f8a7a2b6.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
